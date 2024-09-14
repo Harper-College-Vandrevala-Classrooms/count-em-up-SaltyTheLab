9dc74930d874db24df4865c04872a129e6960ff0
Michael <michaelvargo@outlook.com>
2024-09-13 19:50:24 -0500

    Added decrement and custom starting number

--- src/main/java/com/csc/GroceryCounter.java
+++ src/main/java/com/csc/GroceryCounter.java
@@ -1,5 +1,117 @@
 package com.csc;
 
+import java.util.Scanner;
+
 public class GroceryCounter {
-  // Put your code here!
+  Scanner input = new Scanner(System.in);
+  int ten = 0;
+  int one = 0;
+  int tenths = 0;
+  int hundreths = 0;
+  int overflow = 0;
+
+  GroceryCounter() {
+    // System.out.println("enter your starting value:");
+    // this.ten = input.nextInt();
+    // this.one = input.nextInt();
+    // this.tenths = input.nextInt();
+    // this.hundreths = input.nextInt();
+    // while (this.ten < 0 || this.ten > 9 || this.one < 0 || this.one > 9 ||
+    // this.tenths < 0 || this.tenths > 9
+    // || this.hundreths < 0 || this.hundreths > 9) {
+    // System.out.print("invalid starting number. Please renter:");
+    // this.ten = input.nextInt();
+    // this.one = input.nextInt();
+    // this.tenths = input.nextInt();
+    // this.hundreths = input.nextInt();
+    // }
+  };
+
+  public void ten() {
+    if (this.ten == 9) {
+      this.ten = 0;
+      this.one = 0;
+      this.tenths = 0;
+      this.hundreths = 0;
+      this.overflow += 1;
+    } else
+      this.ten += 1;
+  }
+
+  public void ones() {
+    if (this.one == 9) {
+      this.one = 0;
+      ten();
+    } else
+      this.one += 1;
+  }
+
+  public void hundreths() {
+    if (this.hundreths == 9) {
+      this.hundreths = 0;
+      tenths();
+    } else
+      this.hundreths += 1;
+  }
+
+  public void tenths() {
+    if (this.tenths == 9) {
+      this.tenths = 0;
+      ones();
+    } else
+      this.tenths += 1;
+  }
+
+  public void clear() {
+    this.ten = 0;
+    this.one = 0;
+    this.tenths = 0;
+    this.hundreths = 0;
+    this.overflow = 0;
+  }
+
+  public int number_of_overflows() {
+    return this.overflow;
+  }
+
+  public void decrementtens() {
+    this.ten -= 1;
+  }
+
+  public void decrementones() {
+    this.one -= 1;
+    if (this.one < 0) {
+      decrementtens();
+      this.one = 9;
+    }
+  }
+
+  public void decrementtenths() {
+    this.tenths -= 1;
+    if (this.tenths < 0) {
+      decrementones();
+      this.tenths = 9;
+    }
+  }
+
+  public void decrementhundreths() {
+    this.hundreths -= 1;
+    if (this.hundreths < 0) {
+      decrementtenths();
+      this.hundreths = 9;
+    }
+  }
+
+  public String total() {
+    if (this.ten == 0)
+      return (String.format("$%d.%d%d", this.one, this.tenths, this.hundreths));
+    return (String.format("$%d%d.%d%d", this.ten, this.one, this.tenths, this.hundreths));
+  }
+
+  public static void main(String[] args) {
+    System.out.print("enter your starting value:");
+    GroceryCounter bob = new GroceryCounter();
+    System.out.print(bob.number_of_overflows());
+    System.out.print(bob.total());
+  }
 }
--- src/test/java/com/csc/TestGroceryCounter.java
+++ src/test/java/com/csc/TestGroceryCounter.java
@@ -1,7 +1,6 @@
 package com.csc;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -14,11 +13,47 @@ public class TestGroceryCounter {
     counter = new GroceryCounter();
   }
 
-  // Put your tests here!
-
   @Test
-  void itWorks() {
-    assertEquals(true, true);
+  void teninhcrement() {
+    counter.ten();
+    counter.ten();
+    assertEquals("$20.00", counter.total());
   }
 
+  @Test
+  void tenincrementwithoverflow() {
+    for (int i = 0; i < 100; i++)
+      counter.ten();
+    assertEquals(10, counter.number_of_overflows());
+  }
+
+  @Test
+  void hundredpennies() {
+    for (int i = 0; i < 100; i++) {
+      counter.hundreths();
+    }
+    assertEquals("$1.00", counter.total());
+  }
+
+  @Test
+  void decrement() {
+    counter.hundreths();
+    counter.decrementhundreths();
+    counter.tenths();
+    counter.decrementtenths();
+    counter.ones();
+    counter.decrementones();
+    counter.ten();
+    counter.decrementtens();
+    assertEquals("$0.00", counter.total());
+  }
+
+  @Test
+  void hundred() {
+    for (int i = 0; i < 1; i++)
+      counter.ten();
+    for (int i = 0; i < 150; i++)
+      counter.decrementhundreths();
+    assertEquals("$8.50", counter.total());
+  }
 }
